719085b70b2c09ce03d85044797e05d912c04895
Nathan Ramos <nathancostaand1@hotmail.com>
2020-02-05 00:20:13 -0300

    Add rota para acesso ao componente DepartamentoListaComponent

--- gerenciador-usuarios/src/app/app.component.ts
+++ gerenciador-usuarios/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { ApiService } from './core/services/api.service';
-import {Departamento} from './core/models/departamento';
-
 
 @Component({
   selector: 'app-root',
@@ -9,18 +6,9 @@ import {Departamento} from './core/models/departamento';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private api: ApiService){}
-
-  departamentos = [];
-  departamento:Departamento;
-
-  id: Number = null;
+  constructor(){}
 
   title = 'gerenciador-usuarios';
 
-  testando(){
-    this.api.getById('/api/departamentos/',1).subscribe(a=> this.departamento = a);
-    console.log(this.departamento);
-  }
 
 }
